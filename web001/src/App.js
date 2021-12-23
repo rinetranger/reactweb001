23973402177c499f33a6df6926f516295909c2be
rinetranger <rinetranger@gmail.com>
2021-12-23 14:04:17 +0900

    crav1.0.0

---
+++ web001/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import * as React from 'react';
+
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
+
+
+function App() {
+  return(
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+      </Container>
+    </React.Fragment>
+  )
+}
+
+export default App;
